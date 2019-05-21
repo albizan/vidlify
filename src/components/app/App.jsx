bf0152d0e010520d580a4c9e7413e5195cbd56e8
Alberto Zanotti <albizan@hotmail.it>
2019-05-21 16:34:23 +0200

    Import Movies Component and display it

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Movies from '../movies';
+
 const App = () => {
-  return <main className="container" />;
+  return (
+    <main className="container">
+      <Movies />
+    </main>
+  );
 };
 
 export default App;
