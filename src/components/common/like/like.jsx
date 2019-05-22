0101897ee47068f6400dcfdc8c11ac60c2f23583
Alberto Zanotti <albizan@hotmail.it>
2019-05-22 15:56:10 +0200

    Created like component

---
+++ src/components/common/like/like.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Like = props => {
+  let iconStyle;
+  if (props.liked) {
+    iconStyle = 'fas';
+  } else {
+    iconStyle = 'far';
+  }
+  return (
+    <React.Fragment>
+      <FontAwesomeIcon icon={[iconStyle, 'heart']} />
+    </React.Fragment>
+  );
+};
+
+export default Like;
