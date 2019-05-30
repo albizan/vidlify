ab4e0d971a0d182535da74fc94f900ed59ce1391
Alberto Zanotti <albizan@hotmail.it>
2019-05-30 22:09:27 +0200

    Create TableBody component

---
+++ src/components/common/TableBody/TableBody.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import _ from 'lodash';
+
+const TableBody = props => {
+  const { items, columns } = props;
+  return (
+    <tbody>
+      {items.map(item => (
+        <tr key={item._id}>
+          {columns.map(column => (
+            <td key={column.target || column.key}>{renderCell(item, column)}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+function renderCell(item, column) {
+  // If column has a content function calli it to return a Like/Button
+  if (column.content) return column.content(item);
+  return _.get(item, column.target);
+}
+
+TableBody.propTypes = {
+  items: PropTypes.array.isRequired,
+  columns: PropTypes.array.isRequired,
+};
+
+export default TableBody;
