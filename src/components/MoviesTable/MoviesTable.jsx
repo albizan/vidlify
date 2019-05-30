d8b2d7a6151ef9501a0bfc26e767603aa043c2e2
Alberto Zanotti <albizan@hotmail.it>
2019-05-30 15:04:54 +0200

    Import Like component

--- src/components/MoviesTable/MoviesTable.jsx
+++ src/components/MoviesTable/MoviesTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Like from '../common/like';
+import Like from '../common/Like';
 import TableHeader from '../common/TableHeader';
 
 // Array containing table columns labels
