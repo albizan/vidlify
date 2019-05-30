594dcc92b998bc902ce3fb3140f3ec69bdc87ff0
Alberto Zanotti <albizan@hotmail.it>
2019-05-30 22:23:59 +0200

    Import in icons library sort icons

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,13 +3,13 @@ import './App.css';
 
 // Font-Awesome Icons
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHeart as heartSolid } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as heartSolid, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as heartRegular } from '@fortawesome/free-regular-svg-icons';
 
 // Custom components
 import Movies from '../Movies';
 
-library.add(heartSolid, heartRegular);
+library.add(heartSolid, heartRegular, faSortUp, faSortDown);
 
 const App = () => {
   return (
